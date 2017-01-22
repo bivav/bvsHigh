5853190dd3e642879585133a3ff43cab80090861
Bivav <bivavraj@gmail.com>
2017-01-22 22:44:27 +0530

    Jan 22 Commit

--- app/src/main/java/np/edu/bvs/bvshigh/fragment_attendance.java
+++ app/src/main/java/np/edu/bvs/bvshigh/fragment_attendance.java
@@ -15,7 +15,6 @@ public class fragment_attendance extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
