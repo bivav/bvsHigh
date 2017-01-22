8ff22ea505f292a725ab540a6e2d59fef963668c
Bivav <bivavraj@gmail.com>
2017-01-22 23:03:33 +0530

    Jan 22 Commit

--- app/src/main/java/np/edu/bvs/bvshigh/fragment_alerts.java
+++ app/src/main/java/np/edu/bvs/bvshigh/fragment_alerts.java
@@ -15,7 +15,6 @@ public class fragment_alerts extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
