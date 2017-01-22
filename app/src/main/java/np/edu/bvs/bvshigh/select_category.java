251fe33403e04f721bc3f81969ac67bcc6e87a88
Bivav <bivavraj@gmail.com>
2017-01-22 22:25:28 +0530

    First Commit

--- app/src/main/java/np/edu/bvs/bvshigh/select_category.java
+++ app/src/main/java/np/edu/bvs/bvshigh/select_category.java
@@ -12,11 +12,11 @@ public class select_category extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
-
         Button student_button = (Button)findViewById(R.id.student_button);
         Button teacher_button = (Button)findViewById(R.id.teacher_button);
         Button parents_button = (Button)findViewById(R.id.parents_button);
 
+        // button selects first page
         student_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
